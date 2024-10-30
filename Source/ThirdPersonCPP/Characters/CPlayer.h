c1479a9ed8e40e9b5e7d23942fa868cdd667f4d6
leejoonsoo1 <leejunsu_@nate.com>
2024-10-30 19:51:31 +0900

    2024 10 30
    Init

---
+++ Source/ThirdPersonCPP/Characters/CPlayer.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "CPlayer.generated.h"
+
+UCLASS()
+class THIRDPERSONCPP_API ACPlayer : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	ACPlayer();
+
+protected:
+	virtual void BeginPlay() override;
+
+public:	
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+
+
+};
